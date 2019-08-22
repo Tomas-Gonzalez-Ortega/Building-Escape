b36b13efa1daf26c893cd77b3c1da1de4a4aec9f
Tomas Gonzalez-Ortega <tomas.gonzalez.o@gmail.com>
2019-08-21 17:43:00 -0700

    Second Commit: Proper Geometric Layout of the room, plus some Actors; Room with Door ready to open plus location indicators

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,35 @@
+// Tomas Gonzalez-Ortega
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+    AActor* Owner = GetOwner(); // Finds the owning Actor
+    FRotator NewRotation = FRotator(0.0f, 60.0f, 0.0f); // This rotates the door in 60°
+    Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
