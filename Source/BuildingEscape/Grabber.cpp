4cfa9cb533ee7c18ba3eab4e8ef578b01785a32a
Tomas Gonzalez-Ortega <tomas.gonzalez.o@gmail.com>
2019-09-04 11:36:04 -0700

    Right before Pointers and References changes in video 142

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,9 +1,12 @@
 // Tomas Gonzalez-Ortega
 
-
+#include "Gameframework/Actor.h"
+#include "DrawDebugHelpers.h"
+#include "BuildingEscape.h"
 #include "Grabber.h"
+#define OUT
 
-// Sets default values for this component's properties
+/// Sets default values for this component's properties
 UGrabber::UGrabber()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
@@ -14,21 +17,62 @@ UGrabber::UGrabber()
 }
 
 
-// Called when the game starts
+/// Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+    UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
 }
 
 
-// Called every frame
+/// Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+	// Gets player viewpoint this tick
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+        OUT PlayerViewPointLocation,
+        OUT PlayerViewPointRotation
+    );
+    
+    /// Logs out every tick
+    UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(),
+        *PlayerViewPointRotation.ToString()
+    )
+    
+    FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
+    /// Traces a red line to visualize the players reach with the grabber
+    DrawDebugLine(
+        GetWorld(),
+        PlayerViewPointLocation,
+        LineTraceEnd,
+        FColor(255, 0, 0),
+        false,
+        0.f,
+        0.f,
+        10.f
+    );
+    
+    /// Setup query Parameters
+    FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+    
+    /// Ray-casts out to reach distance
+    FHitResult Hit;
+    GetWorld()->LineTraceSingleByObjectType(
+        OUT Hit,
+        PlayerViewPointLocation,
+        LineTraceEnd,
+        FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+        TraceParameters
+    );
+    
+    /// See what it hits
+    AActor* ActorHit = Hit.GetActor();
+    if(ActorHit)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Line trace hits: %s"), *(ActorHit->GetName()))
+    }
 }
 
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -24,5 +24,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+    float Reach = 100.f; // How far ahead of the player can we reach in cm
 		
 };
