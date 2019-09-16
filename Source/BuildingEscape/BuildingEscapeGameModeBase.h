c408669945f8cec6e1303adaf9d81ae7742756f3
Tomas Gonzalez-Ortega <tomas.gonzalez.o@gmail.com>
2019-09-15 19:09:17 -0700

    Final Commit 2.0

--- Source/BuildingEscape/BuildingEscapeGameModeBase.h
+++ Source/BuildingEscape/BuildingEscapeGameModeBase.h
@@ -1,4 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*
+ BUILDING SCAPE by Tomas Gonzalez-Ortega
+ Vancouver, BC, September 2019
+ Demo based on Udemy - https://www.udemy.com/unrealcourse
+ Unreal Engine C++ Developer: Learn C++ and Make Video Games by Ben Tristem
+ Japanese Shrine Mini asset pack by Bandinanquest
+ Original Soundtrack by Tomas-Gonzalez using Pro Tools 2018.12
+ Door Audio sampled by Tomas-Gonzalez
+*/
 
 #pragma once
 #include "CoreMinimal.h"
--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,4 +1,12 @@
-// Tomas Gonzalez-Ortega
+/*
+ BUILDING SCAPE by Tomas Gonzalez-Ortega
+ Vancouver, BC, September 2019
+ Demo based on Udemy - https://www.udemy.com/unrealcourse
+ Unreal Engine C++ Developer: Learn C++ and Make Video Games by Ben Tristem
+ Japanese Shrine Mini asset pack by Bandinanquest
+ Original Soundtrack by Tomas-Gonzalez using Pro Tools 2018.12
+ Door Audio sampled by Tomas-Gonzalez
+*/
 
 #include "OpenDoor.h"
 #define OUT
